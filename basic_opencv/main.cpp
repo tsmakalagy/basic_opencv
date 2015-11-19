347beb30674e8139aadb2d56183a19982b41ff2b
tsmakalagy <rhfano@gmail.com>
2015-11-19 17:54:24 +0800

    working

--- basic_opencv/main.cpp
+++ basic_opencv/main.cpp
@@ -24,23 +24,44 @@ int main(int argc, char* argv[])
 {
 	Mat image = Mat::zeros( 300, 300, CV_8U );
 	Mat image2 = Mat::zeros( 300, 300, CV_8UC3 );
+	if(argc > 1) {
+		cv::Mat imageMat = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+		if (imageMat.empty())
+		{
+				std::cerr << "ERROR: Could not read image " << argv[1] << std::endl;
+				return 1;
+		}
+
+		//Grayscale matrix
+		cv::Mat grayscaleMat (imageMat.size(), CV_8U);
+
+		//Convert BGR to Gray
+		cv::cvtColor( imageMat, grayscaleMat, CV_BGR2GRAY );
+
+		//Binary image
+		cv::Mat binaryMat(grayscaleMat.size(), grayscaleMat.type());
+
+		//Apply thresholding
+		cv::threshold(grayscaleMat, image, 100, 255, cv::THRESH_BINARY);
+	} else {
+		line( image, Point( 15, 20 ), Point( 70, 20), Scalar( 110, 220, 0 ),  1, 8 );
+	}
    
 	// Draw a line 
 	//line( image, Point( 15, 20 ), /*Point( 20, 80)*/ Point(20, 20), Scalar( 255, 255, 255 ),  1, 4 );
-	line( image, Point( 15, 20 ), Point( 70, 20), Scalar( 110, 220, 0 ),  1, 8 );
-    std::vector<int> coords;		
+	
+    //std::vector<int> coords;		
 	//cout << "M = "<< endl << " "  << image << endl << endl;
-	Mat skeleton_coordinates;
-	cv::findNonZero(image, skeleton_coordinates);	
-	Mat test = getNeighborsCoordinates(15, 20);
+	//Mat skeleton_coordinates;
+	//cv::findNonZero(image, skeleton_coordinates);	
 	//cout << "M = "<< endl << " "  << test << endl << endl;
 	std::vector<Point> t = getEndPointsMat(&image);
 	for (std::vector<Point>::size_type i = 0; i != t.size(); i++)
     {
-		circle(image2, t[i],1.0,Scalar( 255, 0, 255 ), 1, 8);
+		circle(image, t[i],2,Scalar( 255, 0, 255 ), 1, 8);
 		//cout << "(" << t[i].x << "," << t[i].y << ")" << endl;
 	}
-	imshow("Image", image2);
+	imshow("Image", image);
 	waitKey( 0 );
 }
 
@@ -51,43 +72,32 @@ std::vector<Point> getEndPointsMat(Mat *im)
 	Mat m_coordinates;
 	cv::findNonZero(*im, m_coordinates);
 	
-	//cout << "Pos = "<< endl << " "  << m_coordinates << endl << endl;
-	/*for (int i = 1; i < m_coordinates.rows; i++)
-	{
-		int current_x = m_coordinates.at<uchar>(i, 1); // cols
-		int current_y = m_coordinates.at<uchar>(i, 2); // rows
-		Mat neighbor = getNeighborsCoordinates(current_x, current_y);
-		Mat pos= m_coordinates == neighbor;
-		cout << "Pos = "<< endl << " "  << pos << endl << endl;
-		bool eq = cv::countNonZero(m_coordinates != neighbor) == 0;
-		break;
-	}*/
 	m_coordinates.copyTo(skeleton_coordinates);
 	for (std::vector<Point>::iterator it = skeleton_coordinates.begin(); it != skeleton_coordinates.end(); ++it)
     {
 		Point current_coord = Point(it->x, it->y);
 		vector<Point> neighbors = getNeighborsCoordinatesPoint(current_coord);
-		cout << "(" << current_coord.x << "," << current_coord.y << ")  [";
+		//cout << "(" << current_coord.x << "," << current_coord.y << ")  [";
 		for (std::vector<int>::size_type k = 0; k != neighbors.size(); k++)
 		{
-			cout << neighbors[k].x << "," << neighbors[k].y << " ";
+			//cout << neighbors[k].x << "," << neighbors[k].y << " ";
 		}
-		cout << "] ";
+		//cout << "] ";
 		vector<int> pos = compare2vectorsOfPoint(neighbors, skeleton_coordinates);
 		for (std::vector<int>::size_type l = 0; l != pos.size(); l++)
 		{
-			cout << pos[l] << ",";
+			//cout << pos[l] << ",";
 		}
 		cout << endl;
 		//cout << "Pos = " << pos[1] << endl;
 		
-		if (pos.size() == 0)
+		if (pos.size() == 1)
 		{
-			cout << "zero" << endl;
+			//cout << "zero" << endl;
 			endpoints.push_back(current_coord);
 		}
 		else {
-			bool present = false;
+			/*bool present = false;
 			array<int, 3> v_init = {4,5,6};
 			// Test all the pixels around this current pixel
 			for (std::vector<int>::size_type i = 0; i != pos.size(); i++)
@@ -98,19 +108,19 @@ std::vector<Point> getEndPointsMat(Mat *im)
 					continuity[p] += current_pos;
 					if (continuity[p] > 8)
 						continuity[p] = (continuity[p] % 9) + 1;
-					//cout << "Vals = " << continuity[p] << endl;
-					vector<int> mb = compare2vectorsOfInt(continuity, pos);
-					if (mb.size() > 0) {
-						present = true;
-						break;
-					}
 				}
 				
-
+				vector<int> mb = compare2vectorsOfInt(continuity, pos);
+				if (mb.size() > 0) {
+					present = true;
+					break;
+				}
 			}
-			if (present) {
+			if (!present) {
 				endpoints.push_back(current_coord);
-			}
+			}*/
+			//if (pos.size() > 1)
+				//endpoints.push_back(current_coord);
 		}
 		//break;
 	}
